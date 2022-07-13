86ff96ad7cf29d4a0f4f5a3235814c7a30a8ffdb
flavianna <flaviannabe@gmail.com>
2022-07-13 20:56:26 -0300

    Requisito 18 (lint aprovado)

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Requisito 3
 const inpulHeaderEmail = document.getElementById('input-email-header');
 const inputPassword = document.getElementById('input-senha');
 const buttonLogin = document.getElementById('button-form');
@@ -13,3 +14,20 @@ function formularioValue() {
 }
 
 buttonLogin.addEventListener('click', formularioValue);
+
+// Requisito 18
+
+const submit = document.getElementById('submit-btn');
+
+submit.disabled = true;
+
+function enableSub(event) {
+  if (event.target.checked) {
+    submit.disabled = false;
+  } else {
+    submit.disabled = true;
+  }
+}
+
+const agreementCheck = document.getElementById('agreement');
+agreementCheck.addEventListener('change', enableSub);
