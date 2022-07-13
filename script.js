7b1cf8aeb7cfe75f27a85020b86373650e6d5afd
flavianna <flaviannabe@gmail.com>
2022-07-13 16:25:21 -0300

    Requisito 3 (lint aprovado)

---
+++ script.js
@@ -0,0 +1,15 @@
+const inpulHeaderEmail = document.getElementById('input-email-header');
+const inputPassword = document.getElementById('input-senha');
+const buttonLogin = document.getElementById('button-form');
+
+function formularioValue() {
+  const inputEmailValor = inpulHeaderEmail.value;
+  const inputSenhaValor = inputPassword.value;
+  if (inputEmailValor === 'tryber@teste.com' && inputSenhaValor === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+buttonLogin.addEventListener('click', formularioValue);
